5fa767fac0cdda8c13ba5dcc4db6765fa50e22ab
RodrigoSpano <rorrospano17@gmail.com>
2023-08-28 13:46:36 -0300

    header done

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className={`${dark ? 'dark' : ''} transition-all duration-500 ease-soft-spring`}>
-    <div className={`dark:bg-black bg-white transition-background ease-in-out duration-400 min-h-screen`}>
+    <div className={`dark:bg-black bg-slate-100 transition-background ease-in-out duration-400 min-h-screen`}>
       <HeaderContainer />
     </div>
     </div>
--- src/components/header/HeaderContainer.tsx
+++ src/components/header/HeaderContainer.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import NameContainer from './subcomps/NameContainer'
 import TogglesContainer from './subcomps/TogglesContainer'
+import {LuPointer} from 'react-icons/lu'
+import {motion} from 'framer-motion'
 
 const HeaderContainer: React.FC = () => {
   return (
     <div className='min-h-[100vh] flex flex-col items-center justify-center select-none relative'>
       <TogglesContainer />
       <NameContainer />
+      <motion.span className='dark:text-white opacity-75 text-2xl rotate-180 absolute bottom-5 cursor-pointer' whileHover={{scale: 1.3}} animate={{rotate: 180}} whileTap={{ scale: [1,2]}}>
+      <LuPointer />
+      </motion.span>
     </div>
   )
 }
